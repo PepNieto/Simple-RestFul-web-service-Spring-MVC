98e0e94cb8b56e0987bd224ef8b173469c809050
win10 <win10@DESKTOP-QV32PE8.home>
2019-06-24 12:30:10 +0200

    Added

---
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -0,0 +1,39 @@
+package com.example.demo.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.domain.Customer;
+import com.example.demo.repositories.CustomerRepository;
+
+@Component
+public class BootStrapData implements CommandLineRunner{
+
+	private final CustomerRepository customerRepository;
+	
+	public BootStrapData(CustomerRepository customerRepository) {
+		this.customerRepository  = customerRepository;
+	}
+	@Override
+	public void run(String... args) throws Exception{
+		
+		System.out.println("Loading Customer Data");
+	
+		Customer c1 = new Customer();
+		c1.setFirstname("Michael");
+		c1.setLastname("Weston");
+		customerRepository.save(c1);
+		
+		Customer c2 = new Customer();
+		c2.setFirstname("Sam");
+		c2.setLastname("Axe");
+		customerRepository.save(c2);
+	
+		Customer c3 = new Customer();
+		c3.setFirstname("Fiona");
+		c3.setLastname("Shrek");
+		customerRepository.save(c3);
+		
+		System.out.println("Customer Saved " + customerRepository.count());
+	}
+}
--- src/main/java/com/example/demo/domain/Customer.java
+++ src/main/java/com/example/demo/domain/Customer.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 
 
 
-@Data
+
 @Entity
 public class Customer {
 
@@ -18,4 +18,24 @@ public class Customer {
 	private Long id;
 	private String firstname;
 	private String lastname;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	
 }
